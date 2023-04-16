f7e81fe33b366151c80aa3ae4abb1e8f22d6d9af
Jinjie Xu <91656100+jxthetrash@users.noreply.github.com>
2023-04-16 09:06:48 +0000

    margin and padding of button

--- src/components/CupDetails.js
+++ src/components/CupDetails.js
@@ -5,7 +5,7 @@ const CupDetails = ({description, receipeUrl, imageUrl, name})  => {
         <img className="detail-pic" src={imageUrl} alt={name} />
         <div className=".d-flex" id="name-and-button">
             <h2 className="detail-name d-inline">{name}</h2>
-            <a href={receipeUrl} className="d-inline"><button type="button" className="btn btn-outline-secondary">Go To Receipe</button></a>
+            <a href={receipeUrl} className="d-inline pr-5 mr-5"><button type="button" className="btn btn-outline-secondary">Go To Receipe</button></a>
         </div>
         <p className="discription">{description}</p>
         </>
