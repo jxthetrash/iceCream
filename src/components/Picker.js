85035d83e3794e65474975de90f07d58029f719e
Jinjie Xu <91656100+jxthetrash@users.noreply.github.com>
2023-04-16 03:14:16 +0000

    grid change

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -50,7 +50,7 @@ const Picker = () => {
           </div>
         </div>
         <div id="intro">
-          {!Number.isInteger(targetCupIndex) && <h5>The page introduces 12 types of ice creams around the world! Click on the thumbnails to  find out more about diffrent ice creams.</h5>}
+          {!Number.isInteger(targetCupIndex) && <h2>The page introduces 12 types of ice creams around the world! Click on the thumbnails to  find out more about diffrent ice creams.</h2>}
         </div>
       </div>
     </div>)
