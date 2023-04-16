8e180fe6df8e6603fb1b29dac0f7a17b6adccaaf
Jinjie Xu <91656100+jxthetrash@users.noreply.github.com>
2023-04-16 09:17:40 +0000

    header link to home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Picker from "./components/Picker";
 function App() {
   return (
     <div className="App">
-      <header className="App-header sticky-top" id="header">
+      <header className="App-header sticky-top" id="header" onClick={() => window.location.reload(false)}>
         <h1 className="d-flex justify-content-center border-bottom ">THE ICE CREAM STORE</h1>
       </header>
 
